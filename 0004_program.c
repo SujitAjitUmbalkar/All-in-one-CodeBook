dcbb2458e86bfc1f1b3ed2cce3455b34c9035150
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-09-29 13:10:54 +0530

    Refactor percentage program by adding CalculatePercentage function for cleaner logic

--- 0004_program.c
+++ 0004_program.c
@@ -1,29 +1,38 @@
 ///////////////////////////////////////////////////////
 //
-//  File name :     program2.c
+//  File name :     0004_program.c
 //  Descreption :   Used to calculate percentage
-//  Author :        Piyush Manohar Khairnar
+//  Author :        Sujit Ajit Umbalkar
 //  Date :          28/04/2025
 //
 ///////////////////////////////////////////////////////
 
+
 #include<stdio.h>
+
+float CalculatePercentage(int iTotalMarks, int iObtainedMarks)
+{
+    float fPercentageValue = 0.0f;
+    fPercentageValue = ((float)iObtainedMarks / (float)iTotalMarks) * 100;
+    return fPercentageValue;
+}
 
 int main()
 {
-    int Total = 0;
-    int Obtained = 0;
-    float Percentage = 0.0f;
+    int iTotal = 0;
+    int iObtained = 0;
+    float fPercentage = 0.0f;
 
     printf("Enter your total marks : \n");
-    scanf("%d",&Total);
+    scanf("%d",&iTotal);
 
     printf("Enter your obtained marks : \n");
-    scanf("%d",&Obtained);
+    scanf("%d",&iObtained);
 
-    Percentage = ((float)Obtained / (float)Total) * 100;
+    //fPercentage = ((float)iObtained / (float)iTotal) * 100;
+    fPercentage = CalculatePercentage(iTotal,iObtained);
 
-    printf("Percentage is : %.2f%%\n",Percentage);
+    printf("Percentage is : %.2f%%\n",fPercentage);
     
     return 0;
 }
