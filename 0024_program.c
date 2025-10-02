d440713c3a98530a863592324dcd6279f99723f6
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-10-02 10:42:14 +0530

    print counters upto n numbers in increment order

---
+++ 0024_program.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+// Input : 5
+// 1 + 2 + 3 + 4 + 5
+
+int Summation(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%d\t",iCnt);
+    }
+    return 0;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+    
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    Summation(iValue);
+
+    return 0;
+}
