60b23ae668a822a55a0dac581461fdcdb14479be
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-10-05 11:39:19 +0530

    Started Java ... (please refer JAVA_INSTRUCTIONS.md)

---
+++ 0051program.java
@@ -0,0 +1,11 @@
+
+class program_0051
+{
+    public static void main(String A[])
+    {
+        System.out.println("Jay Ganesh...");
+    }
+}
+
+// Compile :    javac program51.java
+// Run :        java program51
