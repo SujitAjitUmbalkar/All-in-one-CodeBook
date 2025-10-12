c1c118ade8ea7ba49fc96c64536c80b8a54f343c
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-10-12 11:35:45 +0530

    Improvement 14 = update 2 in Full Square code

--- 0089_program.c
+++ 0089_program.c
@@ -1,12 +1,20 @@
+// Full Square
+
+//      * * * *
+//      * * * *
+//      * * * *
+//      * * * *
+
+
 #include<stdio.h>
 
 void Display()
 {
     int i = 0, j = 0;
 
-    for(i = 1; i<= 3; i++)
+    for(i = 1; i<= 4; i++)
     {
-        for(j = 1; j <= 3; j++)
+        for(j = 1; j <= 4; j++)
         {
             printf("*\t");
         }
