91dc26435ed69168ca49e66c4a58b691ba22330a
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-10-12 11:32:03 +0530

    Improvement 12 = Full Square

--- 0087_program.c
+++ 0087_program.c
@@ -1,23 +1,43 @@
+// Full Square
+
+//      * * * *
+//      * * * *
+//      * * * *
+//      * * * *
+
+
 #include<stdio.h>
 
 void Display()
 {
+    printf("*\t");      //1
     printf("*\t");
     printf("*\t");
     printf("*\t");
-    
+
     printf("\n");
 
     printf("*\t");
+    printf("*\t");      //2
     printf("*\t");
     printf("*\t");
-    
+
     printf("\n");
+
+    printf("*\t");
+    printf("*\t");      //3
     printf("*\t");
     printf("*\t");
-    printf("*\t");
-    
+
     printf("\n");
+
+    printf("*\t");      //4
+    printf("*\t");
+    printf("*\t");
+    printf("*\t");
+
+    printf("\n");
+
 }
 
 int main()
