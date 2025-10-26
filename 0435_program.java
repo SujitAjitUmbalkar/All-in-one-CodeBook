485793fd36321f8f5d1c1df5468a0856384ecddd
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-10-26 21:00:18 +0530

    using getName() and length() inbuilt functions

--- 0435_program.java
+++ 0435_program.java
@@ -8,5 +8,7 @@ class program_0435
         File fobj = new File("Marvellous.txt");
         System.out.println("File name is : "+fobj.getName());
         System.out.println("File size is : "+fobj.length());
+        System.out.println("File header length is : "+fobj.getName().length());
+         
     }
 }
