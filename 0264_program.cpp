ca78b3ffbb93d4cf26bf8060cd861d4d7b5b52e6
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-10-27 22:08:52 +0530

    Calculate the number of 1 in Binary representaion of any number

--- 0264_program.cpp
+++ 0264_program.cpp
@@ -31,3 +31,45 @@ int main()
     
     return 0;
 }
+
+/*
+
+import java.util.Scanner;
+
+class Program {
+
+    public static int CountOne(int iNo) {
+        int iDigit = 0;
+        int iCount = 0;
+
+        
+        while(iNo != 0) {
+            iDigit = iNo % 2;
+            if(iDigit == 1) {
+                iCount++;
+            }
+           
+            iNo = iNo / 2;
+        }
+        return iCount;
+    }
+
+    public static void main(String[] args) {
+        int iValue = 0, iRet = 0;
+
+        // Create a Scanner object for user input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number : ");
+        iValue = sc.nextInt();
+
+        // Call the function to count the bits
+        iRet = CountOne(iValue);
+
+        System.out.println("Number of 1 : " + iRet);
+        
+        sc.close(); // Close the scanner
+    }
+}
+
+*/
