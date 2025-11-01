aacdfd04af504471e3409e8201bfcf86b6336c3e
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-11-01 19:45:55 +0530

    Turn off two bits at dynamic positions using a combined mask

---
+++ 0287_program.java
@@ -0,0 +1,104 @@
+//  Turn off two bits at dynamic positions using a combined mask
+
+import java.util.Scanner;
+
+class Program
+{
+    public static int OffBitMultiple(int iNo, int iPos1, int iPos2)
+    {
+        int iMask1 = 1;
+        int iMask2 = 1;
+        int iMask = 0;
+        int iResult = 0;
+
+        iMask1 = iMask1 << (iPos1 - 1);
+        iMask2 = iMask2 << (iPos2 - 1);
+        
+        iMask1 = ~iMask1;
+        iMask2 = ~iMask2;
+        
+        iMask = iMask1 & iMask2;
+        
+        iResult = iNo & iMask;
+
+        return iResult;
+    }
+
+    public static void main(String[] args)
+    {
+        int iValue = 0, iLocation1 = 0, iLocation2 = 0;
+        int iRet = 0;
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number : ");
+        iValue = sc.nextInt();
+
+        System.out.println("Enter first position : ");
+        iLocation1 = sc.nextInt();
+
+        System.out.println("Enter second position : ");
+        iLocation2 = sc.nextInt();
+
+        iRet = OffBitMultiple(iValue, iLocation1, iLocation2);
+        System.out.println("Updated number is : " + iRet);
+        
+        sc.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// typedef unsigned int UINT;
+
+// UINT OffBitMultiple(UINT iNo, UINT iPos1, UINT iPos2)
+// {
+//     UINT iMask1 = 1;
+//     UINT iMask2 = 1;
+//     UINT iMask = 0;
+
+//     UINT iResult = 0;
+
+//     iMask1 = iMask1 << (iPos1 - 1);
+//     iMask2 = iMask2 << (iPos2 - 1);
+    
+//     iMask1 = ~iMask1;
+//     iMask2 = ~iMask2;
+    
+//     iMask = iMask1 & iMask2;
+
+//     iResult = iNo & iMask;
+
+//     return iResult;
+// }
+
+// int main()
+// {
+//     UINT iValue = 0,iRet = 0, iLocation1 = 0, iLocation2 = 0;
+
+//     cout<<"Enter number : \n";
+//     cin>>iValue;
+
+//     cout<<"Enter first position : \n";
+//     cin>>iLocation1;
+
+//     cout<<"Enter second position : \n";
+//     cin>>iLocation2;
+
+//     iRet = OffBitMultiple(iValue,iLocation1,iLocation2);
+
+//     cout<<"Updated number is : "<<iRet<<"\n";
+
+//     return 0;
+// }
