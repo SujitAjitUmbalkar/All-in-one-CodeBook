82fea3855becd2b6a8ed8fe8f13fe663e3148126
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-11-08 19:08:32 +0530

    Swap two numbers (Call by Reference )/ c++/ permanant change

--- 0298_program.cpp
+++ 0298_program.cpp
@@ -22,3 +22,12 @@ int main()
     
     return 0;
 }
+
+
+// It does the exact same thing as Call by Address (permanent changes), but it's cleaner:
+
+// No pointers: You don't have to deal with * and & everywhere.
+
+// Easier to read: It looks just like normal variables.
+
+// Safer: References cannot be NULL (empty), whereas pointers can be, which can cause crashes.
